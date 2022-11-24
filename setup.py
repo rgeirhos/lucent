dcac45de0c559110f6cd30d3caeb62972ac8e33c
Robert Geirhos <23079422+rgeirhos@users.noreply.github.com>
2022-11-24 15:13:04 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="torch-lucent-rg-v3",
+    name="torch-lucent-rg-v4",
     packages=find_packages(exclude=[]),
     version=version,
     description=(
