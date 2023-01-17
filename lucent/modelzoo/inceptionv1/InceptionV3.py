6575cf6f662021a79ac1288880632d254c3f7eb3
Robert Geirhos <23079422+rgeirhos@users.noreply.github.com>
2023-01-17 14:44:43 -0500

    Updated InceptionV3 with classifier

--- lucent/modelzoo/inceptionv1/InceptionV3.py
+++ lucent/modelzoo/inceptionv1/InceptionV3.py
@@ -146,6 +146,8 @@ class InceptionV3(nn.Module):
         self.softmax2_pre_activation_matmul = nn.Linear(in_features=1024, out_features=1008, bias=True)
         
         if self.add_custom_layers:
+            self.classifier = SimpleCNN()
+            self.sigmoid = torch.sigmoid()
             self.lyr_1 = torch.nn.Linear(in_features=1008, out_features=1001, bias=True)
             self.lyr_2 = torch.nn.Linear(in_features=1001, out_features=2000, bias=True)
             self.lyr_3 = torch.nn.Linear(in_features=2000, out_features=1000, bias=True)
@@ -425,14 +427,19 @@ class InceptionV3(nn.Module):
         softmax2 = self.softmax2(softmax2_pre_activation_matmul)
         
         if self.add_custom_layers: #R
+            
+            classifier_logits = self.classifier(x)
+            classifier_softmax = self.sigmoid(classifier_logits)
         
             if self.use_RELU_in_custom_layers:
                 lyr_1_pre_relu = self.lyr_1(softmax2_pre_activation_matmul)
                 lyr_1_relu_output = self.lyr_1_relu(lyr_1_pre_relu)
+                lyr_1_relu_output[1000] = classifier_softmax
                 lyr_2_pre_relu = self.lyr_2(lyr_1_relu_output)
                 lyr_2_relu_output = self.lyr_2_relu(lyr_2_pre_relu)
                 lyr_3_output = self.lyr_3(lyr_2_relu_output)
             else:
+                raise NotImplementedError("classifier not used here")
                 lyr_1_pre_relu = self.lyr_1(softmax2_pre_activation_matmul)
                 lyr_2_pre_relu = self.lyr_2(lyr_1_pre_relu)
                 lyr_3_output = self.lyr_3(lyr_2_pre_relu)
